0ebe4431bfccbf74257c2ba406750e293e3f41fd
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2018-11-21 14:51:15 -0500

    Validacion de actividades 2
    ->Kardex
    ->Condiciones locativas
    ->Formulas de despacho
    ->Papelerias de consignacion
    ->Remiciones

--- app/Http/Controllers/Api/Auth/Supervisores/ActividadesController.php
+++ app/Http/Controllers/Api/Auth/Supervisores/ActividadesController.php
@@ -10,11 +10,31 @@ class ActividadesController extends ValidarActividadesController
 {
     public function index(Request $request)
     {   
-        //guada el nombre de la tabla de la actividad
-        $tabla = request('nombre_tabla');
-        //valida la tabla y llama la funcion del controlador
-        $validar=$this->$tabla($request);
-        return $validar;
+        //validacion de los datos de la actividad
+        $validator=\Validator::make($request->all(),[
+            'id_plan_trabajo' => 'required',
+            'id_actividad' => 'required',
+            'observaciones' => 'required',
+            'calificacion' => 'required',
+            'calificacion_pv' => 'required',
+            'nombre_tabla' => 'required',
+    
+        ]);
+        if($validator->fails())
+        {
+          return response()->json( $errors=$validator->errors()->all() );
+        }
+
+        else
+        {
+
+             //guada el nombre de la tabla de la actividad
+            $tabla = request('nombre_tabla');
+            //valida la tabla y llama la funcion del controlador
+            $validar=$this->$tabla($request);
+            return $validar;
+        }
+       
  
     }
 }
--- app/Http/Controllers/ValidarActividadesController.php
+++ app/Http/Controllers/ValidarActividadesController.php
@@ -3,23 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Apertura;
+use App\Modelos\Actividades\Apertura;
+use App\Modelos\Actividades\CondicionesLocativas;
+use App\Modelos\Actividades\PapeleriaConsignaciones;
+use App\Modelos\Actividades\FormulasDespachos;
+use App\Modelos\Actividades\Remisiones;
+use App\Modelos\Actividades\Kardex;
 
 class ValidarActividadesController extends Controller
 {
-    //requerimiento de datos para crear la finalizacion de la actividad
+    //requerimiento de datos para llenar la actividad
     public function apertura($request)
     {
         //validacion de los datos de la actividad
         $validator=\Validator::make($request->all(),[
-            'id_apertura' => 'required',
-            'id_plan_trabajo' => 'required',
-            'fecha_inicio' => 'required|date_format:Y-m-d H:i:s',
-            'fecha_fin' => 'required|date_format:Y-m-d H:i:s',
-            'fecha_mod' => 'required|date_format:Y-m-d H:i:s',
+            'id_actividad' => 'required',
             'observaciones' => 'required',
-            'id_prioridad' => 'required',
-            'estado' => 'required',
             'calificacion' => 'required',
             'nombre_tabla' => 'required',
             'calificacion_pv' => 'required',
@@ -34,11 +33,11 @@ class ValidarActividadesController extends Controller
         {
 
             //actualizacion de la actividad por el supervisor
-            $actividad = Apertura::find(request('id_apertura'));
+            $actividad = Apertura::where('id_plan_trabajo', request('id_plan_trabajo'))->find(request('id_actividad'));
             if($actividad!= null){
-                $actividad->fecha_mod = request('fecha_mod');
+                $actividad->fecha_mod =  date('Y-m-d H:i:s');
                 $actividad->observaciones = request('observaciones');
-                $actividad->estado = request('estado');
+                $actividad->estado = 'completo';
                 $actividad->calificacion = request('calificacion');
                 $actividad->calificacion_pv = request('calificacion_pv');
                 $actividad->update();
@@ -47,4 +46,182 @@ class ValidarActividadesController extends Controller
             return response()->json(['message' => 'Error Actividad no encontrada']);
         }
     }
+
+     //requerimiento de datos para la actividad
+     public function condiciones_locativas($request)
+     {
+         //validacion de los datos de la actividad
+         $validator=\Validator::make($request->all(),[
+             'id_actividad' => 'required',
+             'calificacion' => 'required',
+             'observaciones' => 'required',
+             'calificacion_pv' => 'required',
+     
+         ]);
+         if($validator->fails())
+         {
+           return response()->json( $errors=$validator->errors()->all() );
+         }
+ 
+         else
+         {
+ 
+             //actualizacion de la actividad por el supervisor
+             $actividad = CondicionesLocativas::where('id_plan_trabajo', request('id_plan_trabajo'))->find(request('id_actividad'));
+             if($actividad!= null){
+                 $actividad->fecha_mod = date('Y-m-d H:i:s');
+                 $actividad->observacion = request('observaciones');
+                 $actividad->estado = 'completo';
+                 $actividad->calificacion = request('calificacion');
+                 $actividad->calificacion_pv = request('calificacion_pv');
+                 $actividad->update();
+                 return response()->json(['message' => 'Actividad realizada con exito']);
+             }
+             return response()->json(['message' => 'Error Actividad no encontrada']);
+         }
+     }
+
+     //requerimiento de datos para la actividad
+     public function formulas_despachos($request)
+     {
+         //validacion de los datos de la actividad
+         $validator=\Validator::make($request->all(),[
+             'id_actividad' => 'required',
+             'calificacion' => 'required',
+             'observaciones' => 'required',
+             'calificacion_pv' => 'required',
+     
+         ]);
+         if($validator->fails())
+         {
+           return response()->json( $errors=$validator->errors()->all() );
+         }
+ 
+         else
+         {
+ 
+             //actualizacion de la actividad por el supervisor
+             $actividad = FormulasDespachos::where('id_plan_trabajo', request('id_plan_trabajo'))->find(request('id_actividad'));
+             if($actividad!= null){
+                 $actividad->fecha_mod = date('Y-m-d H:i:s');
+                 $actividad->observacion = request('observaciones');
+                 $actividad->estado = 'completo';
+                 $actividad->calificacion = request('calificacion');
+                 $actividad->calificacion_pv = request('calificacion_pv');
+                 $actividad->update();
+                 return response()->json(['message' => 'Actividad realizada con exito']);
+             }
+             return response()->json(['message' => 'Error Actividad no encontrada']);
+         }
+     }
+
+     //requerimiento de datos para la actividad
+     public function kardex($request)
+     {
+         //validacion de los datos de la actividad
+         $validator=\Validator::make($request->all(),[
+             'id_actividad' => 'required',
+             'calificacion' => 'required',
+             'observaciones' => 'required',
+             'calificacion_pv' => 'required',
+     
+         ]);
+         if($validator->fails())
+         {
+           return response()->json( $errors=$validator->errors()->all() );
+         }
+ 
+         else
+         {
+ 
+             //actualizacion de la actividad por el supervisor
+             $actividad = Kardex::where('id_plan_trabajo', request('id_plan_trabajo'))->find(request('id_actividad'));
+             if($actividad!= null){
+                 $actividad->fecha_mod = date('Y-m-d H:i:s');
+                 $actividad->observacion = request('observaciones');
+                 $actividad->estado = 'completo';
+                 $actividad->calificacion = request('calificacion');
+                 $actividad->calificacion_pv = request('calificacion_pv');
+                 $actividad->update();
+                 return response()->json(['message' => 'Actividad realizada con exito']);
+             }
+             return response()->json(['message' => 'Error Actividad no encontrada']);
+         }
+     }
+
+     //requerimiento de datos para la actividad
+     public function papeleria_consignaciones($request)
+     {
+         //validacion de los datos de la actividad
+         $validator=\Validator::make($request->all(),[
+             'id_actividad' => 'required',
+             'papeleria' => 'required',
+             'valor_consignacion' => 'required',
+             'valor_faltante' => 'required',
+             'valor_sobrante' => 'required',
+             'calificacion' => 'required',
+             'observaciones' => 'required',
+             'calificacion_pv' => 'required',
+     
+         ]);
+         if($validator->fails())
+         {
+           return response()->json( $errors=$validator->errors()->all() );
+         }
+ 
+         else
+         {
+ 
+             //actualizacion de la actividad por el supervisor
+             $actividad = PapeleriaConsignaciones::where('id_plan_trabajo', request('id_plan_trabajo'))->find(request('id_actividad'));
+             if($actividad!= null){
+                 $actividad->fecha_mod = date('Y-m-d H:i:s');
+                 $actividad->observacion = request('observaciones');
+                 $actividad->papeleria = request('papeleria');
+                 $actividad->valor_consignacion = request('valor_consignacion');
+                 $actividad->valor_faltante = request('valor_faltante');
+                 $actividad->valor_sobrante = request('valor_sobrante');
+                 $actividad->estado = 'completo';
+                 $actividad->calificacion = request('calificacion');
+                 $actividad->calificacion_pv = request('calificacion_pv');
+                 $actividad->update();
+                 return response()->json(['message' => 'Actividad realizada con exito']);
+             }
+             return response()->json(['message' => 'Error Actividad no encontrada']);
+         }
+     }
+
+     //requerimiento de datos para la actividad
+     public function remisiones($request)
+     {
+         //validacion de los datos de la actividad
+         $validator=\Validator::make($request->all(),[
+             'id_actividad' => 'required',
+             'calificacion' => 'required',
+             'observaciones' => 'required',
+             'calificacion_pv' => 'required',
+     
+         ]);
+         if($validator->fails())
+         {
+           return response()->json( $errors=$validator->errors()->all() );
+         }
+ 
+         else
+         {
+ 
+             //actualizacion de la actividad por el supervisor
+             $actividad = Remisiones::where('id_plan_trabajo', request('id_plan_trabajo'))->find(request('id_actividad'));
+             if($actividad!= null){
+                 $actividad->fecha_mod = date('Y-m-d H:i:s');
+                 $actividad->observacion = request('observaciones');
+                 $actividad->estado = 'completo';
+                 $actividad->calificacion = request('calificacion');
+                 $actividad->calificacion_pv = request('calificacion_pv');
+                 $actividad->update();
+                 return response()->json(['message' => 'Actividad realizada con exito']);
+             }
+             return response()->json(['message' => 'Error Actividad no encontrada']);
+         }
+     }
 }

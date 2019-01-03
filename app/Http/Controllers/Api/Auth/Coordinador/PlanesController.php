07a15fbb9e052916ad8fd23d24d0d111f554f5c3
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-01-03 14:48:53 -0500

    fixed
    ->quitar nombre de la sucursal al mostrar el array de actividades

--- app/Http/Controllers/Api/Auth/Coordinador/PlanesController.php
+++ app/Http/Controllers/Api/Auth/Coordinador/PlanesController.php
@@ -111,6 +111,63 @@ class PlanesController extends Controller
                     //  generar el array con el listado de actividades pendientes en la semana
                     foreach($fe as $fecha){
                         $fecha->nombre_actividad = $ac->nombre_actividad;
+                        array_push($lista_actividades_arr, $fecha);
+                    }
+                    
+                    
+                }
+                    // validar si el array tiene actividades
+                    return response()->json(['Actividades' => $lista_actividades_arr, 'Nombre' => $plan->nombre, 'Sucursal' => $plan->id_sucursal],200);
+                
+            }else{
+                return response()->json('No tienes permiso para acceder a esta ruta',400);
+            }
+            
+        }
+    }
+
+    public function deletePlan(Request $request)
+    {
+        $validator=\Validator::make($request->all(),[
+            'id_plan_trabajo' => 'required',
+        ]);
+
+        if($validator->fails())
+        {
+          return response()->json( $errors=$validator->errors()->all(),400 );
+        }
+
+        else
+        {
+            $user=DB::table('users as u')->where('u.id','=',Auth::id())->first();
+            $coordinador=DB::table('coordinadores')->where('cedula',$user->cedula)->first();
+
+            if($coordinador != null){
+                //obtener las actividades segun su plan de trabajo 
+                $actividades=DB::table('plan_trabajo_asignacion as p')
+                ->select('su.nombre as nombreSucursal', 'p.nombre as nombrePlan', 'ac.nombre_tabla', 'p.id_plan_trabajo', 'ac.nombre_actividad')
+                ->join('actividades as ac','p.id_plan_trabajo','ac.id_plan_trabajo')
+                ->join('sucursales as su','p.id_sucursal','su.id_suscursal')
+                ->where('p.id_plan_trabajo',request('id_plan_trabajo'))
+                ->where('p.idcoordinador',$coordinador->id_cordinador)
+                ->orderby('ac.id_plan_trabajo','desc')
+                ->get();
+
+                $plan = DB::table('plan_trabajo_asignacion as p')
+                ->where('p.id_plan_trabajo', request('id_plan_trabajo'))
+                ->first();
+    
+                //array que almacenará las actividanes correspondientes a los 7 dias despues del dia actual 
+                $lista_actividades_arr = array();
+                //bucle que itera las actividades y las obtiene segun el plan de trabajo
+                foreach($actividades as $ac){
+                    $fe = DB::table($ac->nombre_tabla. ' as ac')
+                    ->where('ac.id_plan_trabajo',$ac->id_plan_trabajo)
+                    ->get();
+    
+                    //  generar el array con el listado de actividades pendientes en la semana
+                    foreach($fe as $fecha){
+                        $fecha->nombre_actividad = $ac->nombre_actividad;
                         array_push($lista_actividades_arr, [$ac->nombreSucursal =>$fecha]);
                     }
                     

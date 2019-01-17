c1813da6d0c96aa9263f73ba41cc23c68f9e78a0
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-01-17 10:06:29 -0500

    documentacion legal
    ->listar los documentos legales

---
+++ app/Http/Controllers/Api/Auth/Coordinador/TablasDetalles/DocumentacionController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth\Coordinador\TablasDetalles;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Modelos\Actividades\DocumentacionLegal;
+use Illuminate\Support\Facades\DB;
+
+class DocumentacionController extends Controller
+{
+    public function documentacion_legal(Request $request)
+     {
+         //validacion de los datos de la actividad
+         $validator=\Validator::make($request->all(),[
+             'id_actividad' => 'required',
+             'documento_vencido' => 'required',
+             'documento_renovado' => 'required',
+             'observaciones' => 'required',
+
+         ]);
+         if($validator->fails())
+         {
+           return response()->json( ['message' => $validator->errors()->all()],400);
+         }
+
+         else
+         {
+
+             //actualizacion de la actividad por el supervisor
+             $actividad = DB::table('documentacion_actividad')
+             ->where('id_actividad', request('actividad'))
+             ->where('id_documento', request('id_documento'))
+             ->first();
+             if($actividad!= null){
+                 $img_vencido = 'documento_vencido' . time();
+                 $img_renovado = 'documento_renovado' . time();
+                 
+
+                 if (request('documento_vencido') != "" && request('documento_renovado') != "") { // storing image in storage/app/public Folder
+                    if(strpos(request('documento_vencido'), 'supervisores_api/storage/app/public/img') == false ){
+                        Storage::disk('public')->put('img/'.$img_vencido, base64_decode(request('documento_vencido')));
+                        $actividad->documento_vencido = $img_vencido;
+                    }
+
+                    if(strpos(request('documento_renovado'), 'supervisores_api/storage/app/public/img') == false ){
+                        Storage::disk('public')->put('img/'.$img_renovado, base64_decode(request('documento_renovado')));
+                        $actividad->documento_renovado = $img_renovado;
+                    }
+                }
+                $actividad->observaciones = request('observaciones');
+                $actividad->update();
+                return response()->json(['message' => 'Documento registrado']);
+
+                }
+             return response()->json(['message' => 'Error Actividad no encontrada']);
+         }
+     }
+
+     public function listarDocumentos(Request $request)
+     {
+         //validacion de los datos de la actividad
+         $validator=\Validator::make($request->all(),[
+             'id_actividad' => 'required',
+
+         ]);
+         if($validator->fails())
+         {
+           return response()->json( ['message' => $validator->errors()->all()],400);
+         }
+         else
+         {
+            $documentos = DB::table('documentacion_actividad')
+            ->where('id_actividad', request('id_actividad'))
+            ->get();
+            
+            return response()->json($documentos, 200);
+         }
+    }
+}

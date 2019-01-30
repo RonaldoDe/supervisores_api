cecafbded0724b0e9de5aa8b7c0b9384a71a4b8e
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-01-30 09:51:17 -0500

    agrgar el ptc a multiactividades

--- app/Http/Controllers/Api/Auth/Coordinador/Actividades/MultiActividadController.php
+++ app/Http/Controllers/Api/Auth/Coordinador/Actividades/MultiActividadController.php
@@ -1173,4 +1173,77 @@ class MultiActividadController extends Controller
 
     }
 
+    public function actividades_ptc(Request $request){
+
+        //imporante el id del plana detrabajo debe estar creado a la hora de crear las actividades a dicho plan de trabajo
+
+        $validator=\Validator::make($request->all(),[
+            'id_prioridad' => 'required|numeric',
+            'titulo' => 'required',
+            'id_plan_trabajo'=>'required|numeric',
+            'fecha_inicio'=>'date_format:"Y-m-d"|required',
+            'fecha_fin'=>'date_format:"Y-m-d"|required'
+
+        ]);
+        if($validator->fails())
+        {
+          return response()->json( $errors=$validator->errors()->all(),400 );
+        }
+
+        else
+        {
+
+
+
+        //instancia del modelo documentacion legal para crear un registro de esta tabla
+
+            $fecha= date('Y-m-d');
+
+            if(request('fecha_inicio')>=$fecha && request('fecha_inicio')<=request('fecha_fin')){
+
+
+                $fechas_base_datos=DB::table('actividades_ptc')
+        ->select('fecha_inicio','id_plan_trabajo','fecha_fin')
+        ->where('id_plan_trabajo',request('id_plan_trabajo'))
+        ->get();
+
+        $fecha_ini=request('fecha_inicio');
+        $fecha_finn=request('fecha_fin');
+        //funcion que valida las fechas a insertar en la base de dato hay que colocar esta funcion en las actividades qe
+        //no son tan frecuentes y hay que hacer la funcion para os planes de trabajos que son frecuentes
+        $respuesta=$this->validarQuenoExistanFechasRepetidadEnLaBase($fechas_base_datos,$fecha_ini,$fecha_finn);
+
+
+        if($respuesta>0){
+            return response()->json (["Ya existen estas fechas registradas en esta actividad con este plan de trabajo en la base de datos."],400);
+
+        }else{
+                $ptc =ActividadPtc::create([
+
+                    'id_plan_trabajo' =>request('id_plan_trabajo'),
+                    'titulo' =>request('titulo'),
+                    'fecha_inicio' =>request('fecha_inicio'),
+                    'fecha_fin' =>request('fecha_fin').' '.'23:59:00',
+                    'observacion'=>'',
+                    'id_prioridad' =>request('id_prioridad'),
+                    'estado' =>'Activo',
+
+                ]);
+        }
+
+                // DB::commit();
+
+                return response()->json(["success"=>" Actividad Ptc creada", 'id' => $ptc->id],201);
+
+            }else{
+                return response()->json(["La fecha inicial debe ser mayor o igual a la fecha actual y menor o igual a la fecha final"],400);
+
+            }
+
+
+
+        }
+
+    }
+
 }

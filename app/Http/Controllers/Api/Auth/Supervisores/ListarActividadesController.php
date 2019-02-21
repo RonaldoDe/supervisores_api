6c47924513aeed93172af1bfa5a6618c8c3d4704
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-02-21 16:10:07 -0500

    Update ListarActividadesController.php

--- app/Http/Controllers/Api/Auth/Supervisores/ListarActividadesController.php
+++ app/Http/Controllers/Api/Auth/Supervisores/ListarActividadesController.php
@@ -47,7 +47,7 @@ class ListarActividadesController extends Controller
     //bucle que itera las actividades y las obtiene segun el plan de trabajo
     foreach($actividades as $ac){
         $fe = DB::table($ac->nombre_tabla. ' as ac')
-         ->select('ac.fecha_inicio', 'ac.fecha_fin', 'ac.id_plan_trabajo')
+         ->select('ac.id','ac.fecha_inicio', 'ac.fecha_fin', 'ac.id_plan_trabajo')
          ->where('ac.id_estado',1)
          ->where('ac.id_plan_trabajo',$ac->id_plan_trabajo)
          ->get();

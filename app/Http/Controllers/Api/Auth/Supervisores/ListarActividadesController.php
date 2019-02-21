76d66f58362ddc59b6e899599c3bc5ccbc6d84c4
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-02-21 16:18:24 -0500

    Update ListarActividadesController.php

--- app/Http/Controllers/Api/Auth/Supervisores/ListarActividadesController.php
+++ app/Http/Controllers/Api/Auth/Supervisores/ListarActividadesController.php
@@ -55,7 +55,7 @@ class ListarActividadesController extends Controller
          $count++;
         //  generar el array con el listado de actividades pendientes en la semana
         foreach($fe as $fecha){
-            if($fecha->id_plan_trabajo == $ac->id_plan_trabajo && $fecha->fecha_fin <= $semanal.' 23:59:00' && date('Y-m-d').' 00:00:00' >= $fecha->fecha_inicio){
+            if($fecha->id_plan_trabajo == $ac->id_plan_trabajo && $fecha->fecha_inicio <= $semanal.' 23:59:00' && date('Y-m-d').' 00:00:00' >= $fecha->fecha_inicio){
                 $fecha->nombre_actividad = $ac->nombre_actividad.'-'.$fecha->id;
                 array_push($lista_actividades_arr, [$ac->nombre =>$fecha]);
                 

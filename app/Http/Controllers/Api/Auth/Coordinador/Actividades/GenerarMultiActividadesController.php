388c18737f5b4c9d3a95b74c36fd8917560c0b43
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-02-25 16:08:15 -0500

    Update GenerarMultiActividadesController.php

--- app/Http/Controllers/Api/Auth/Coordinador/Actividades/GenerarMultiActividadesController.php
+++ app/Http/Controllers/Api/Auth/Coordinador/Actividades/GenerarMultiActividadesController.php
@@ -101,21 +101,21 @@ class GenerarMultiActividadesController extends MultiActividadController
                                         if(count($validarDuplicadoFechas) > 0){
                                             foreach ($validarDuplicadoFechas as $fechasDuplicadas) {
                                                 if(request('fecha_inicio').' 00:00:00' >= $fechasDuplicadas->fecha_inicio && request('fecha_inicio').' 00:00:00' <= $fechasDuplicadas->fecha_fin){
-                                                    return response()->json(['La fecha inicio se encunetra en el rango de fechas de otra actividad igual'. $actividad->nombre_tabla],400);
+                                                    return response()->json(['La fecha inicio se encunetra en el rango de fechas de otra actividad '. $actividad->nombre],400);
                                                     
                                                 }
 
                                                 if(request('fecha_fin').' 23:59:00' >= $fechasDuplicadas->fecha_inicio && request('fecha_fin').' 23:59:00' <= $fechasDuplicadas->fecha_fin){
-                                                    return response()->json(['La fecha fin se encunetra en el rango de fechas de otra actividad igual'. $actividad->nombre_tabla],400);
+                                                    return response()->json(['La fecha fin se encunetra en el rango de fechas de otra actividad '. $actividad->nombre],400);
                                                 }
 
                                                 if($fechasDuplicadas->fecha_inicio >= request('fecha_inicio').' 00:00:00' && $fechasDuplicadas->fecha_inicio <= request('fecha_fin').' 23:59:00'){
-                                                    return response()->json(['Ya existe una actividad que se encunetra en el rango de fechas'. $actividad->nombre_tabla],400);
+                                                    return response()->json(['Ya existe una actividad que se encunetra en el rango de fechas '. $actividad->nombre],400);
                                                     
                                                 }
 
                                                 if($fechasDuplicadas->fecha_fin >= request('fecha_inicio').' 23:59:00' && $fechasDuplicadas->fecha_fin <= request('fecha_fin').' 23:59:00'){
-                                                    return response()->json(['Ya existe una actividad que se encunetra en el rango de fechas'. $actividad->nombre_tabla],400);                                                    
+                                                    return response()->json(['Ya existe una actividad que se encunetra en el rango de fechas '. $actividad->nombre],400);                                                    
                                                 }
                                             }
                                         }

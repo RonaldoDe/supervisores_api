92d2a703d082d6cb7e80dea6cbc7b32a60d87d95
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-03-08 11:00:01 -0500

    fixed

--- app/Http/Middleware/CoordinadoresMiddleware.php
+++ app/Http/Middleware/CoordinadoresMiddleware.php
@@ -26,6 +26,13 @@ class CoordinadoresMiddleware
          ->where('c.id_estado','=', 1)
          ->get();
 
+         if(!$coordinador){
+            $usuario = DB::table('usuario as u')->where('u.id_usuario','=',$user->id)->first();
+            if($usuario){
+               $admin = DB::table('usuario_roles')->where('id_usuario','=',$usuario->id_usuario)->where('id_rol', 2)->first();
+            }
+       }
+
          //Acceder al tipo de usuario que pertenece
         $usuarios_roles=Db::table('usuarios_roles as u')
                            ->join('usuario as us','us.id_usuario','=','u.id_usuario')
@@ -34,7 +41,7 @@ class CoordinadoresMiddleware
                            ->first();
 
          //validar el tipo de usuario y madar a una ruta definida
-         if(count($coordinador)>0)
+         if(count($coordinador)>0 || $admin)
          {
             return $next($request);
          }else{

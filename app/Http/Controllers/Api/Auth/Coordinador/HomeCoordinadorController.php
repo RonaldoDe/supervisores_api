39c6c63b14779f566cbd8126910fb9264c5d03c1
RonaldoDe <43652232+RonaldoDe@users.noreply.github.com>
2019-03-08 17:03:44 -0500

    fixed

--- app/Http/Controllers/Api/Auth/Coordinador/HomeCoordinadorController.php
+++ app/Http/Controllers/Api/Auth/Coordinador/HomeCoordinadorController.php
@@ -246,6 +246,19 @@ class HomeCoordinadorController extends Controller
         {
             $user=DB::table('users as u')->where('u.id','=',Auth::id())->first();
             $coordinador=DB::table('coordinadores')->where('correo','=',$user->email)->first();
+            if(!$coordinador){
+                $usuario = DB::table('usuario as u')->where('u.correo','=',$user->email)->where('id_estado','=', 1)->first();
+                if($usuario){
+                    $admin = DB::table('usuarios_roles')->where('id_usuario','=',$usuario->id_usuario)->where('id_rol', 2)->first();
+                    if($admin){
+                        return response()->json('Usuario Administrador', 309);
+                    }else{
+                        return response()->json('Usuario No es administrador', 400);
+                    }
+                }else{
+                    return response()->json('Usuario no econtrado', 400);
+                }
+           }
 
             if($coordinador != null){
                 $zona = DB::table('usuario_zona as z')
